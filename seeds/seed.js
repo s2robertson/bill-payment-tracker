27d679eaa0fcf3fd5cd8c131d2b77261ae359772
Gabriel Parada <gabrielparada05@gmail.com>
2023-06-14 10:42:53 -0400

    seed files -  working ok

---
+++ seeds/seed.js
@@ -0,0 +1,49 @@
+const sequelize = require('../config/connection');
+const { User, Payment, Credit, Bill } = require('../models');
+
+const userData = require('./userData.json');
+const billData = require('./billData.json');
+const creditData = require('./creditData.json');
+const paymentData = require('./paymentData.json');
+
+const seedDatabase = async () => {
+  try {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    for (const bill of billData) {
+      await Bill.create({
+        ...bill,
+        // user_id: users[Math.floor(Math.random() * users.length)].id,
+      });
+    }
+
+    for (const credit of creditData) {
+      await Credit.create({
+        ...credit,
+        // user_id: users[Math.floor(Math.random() * users.length)].id,
+        // post_id: Math.floor(Math.random() * billData.length) + 1,
+      });
+    }
+
+    for (const payment of paymentData) {
+      await Payment.create({
+        ...payment,
+        // user_id: users[Math.floor(Math.random() * users.length)].id,
+        // post_id: Math.floor(Math.random() * billData.length) + 1,
+      });
+    }
+
+    console.log('Database seeded successfully!');
+    process.exit(0);
+  } catch (err) {
+    console.error('Error seeding database:', err);
+    process.exit(1);
+  }
+};
+
+seedDatabase();
