60b3af1c3985967417e85e8cb0c8b2a991dda7a8
Gabriel Parada <gabrielparada05@gmail.com>
2023-06-15 13:42:52 -0400

    bill view and routes deleted

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,11 @@ const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboardRoutes');
 const paymentR = require('./paymentRoutes');
 
-<<<<<<< HEAD
 
 router.use('/', dashboardRoutes);
 router.use('/', paymentR);
-=======
-// router.use('/', dashboardRoutes);
->>>>>>> ce06fec0fd0a665648f0481dbdf69e717beff336
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
+
+// router.use('/', homeRoutes);
 
 module.exports = router;
