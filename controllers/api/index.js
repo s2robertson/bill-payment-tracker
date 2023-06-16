18c9dca6f9d21ef37211c89dfad4d8698c05d0f7
Steven Robertson <s2robertson@gmail.com>
2023-06-15 21:23:59 -0400

    added reminders to dashboard

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const billRoutes = require('./billRoutes');
 const paymentRoutes = require('./paymentRoutes');
-
+const reminderRoutes = require('./reminderRoutes');
 
 router.use('/users', userRoutes);
 router.use('/bills', billRoutes);
 router.use('/payments', paymentRoutes);
-
-
+router.use('/reminders', reminderRoutes);
 
 module.exports = router;
